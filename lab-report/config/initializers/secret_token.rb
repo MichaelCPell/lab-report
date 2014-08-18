9336dfd56319d27e9ccde4ab234be279ede06593
Michael Pell <michaelcpell@gmail.com>
2014-08-18 17:30:22 -0600

    Swap rails 3 for 4

--- lab-report/config/initializers/secret_token.rb
+++ lab-report/config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-LabReport::Application.config.secret_token = '5db63ea9c4ed890e26370c916661fc6bec1cc444cea5b4329dacff33988023da88fe1bbff599452383d60ba00913ac63996cdb93e408bf723bf15e4725429419'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LabReport::Application.config.secret_key_base = '6b3c528355564f73b38e5be714c92b0f147d87ce0f2b334d7d36827db8a37788efed409357631be3875d767b13428bbaf21d6daddb37441bcccd7d402acfe7db'
